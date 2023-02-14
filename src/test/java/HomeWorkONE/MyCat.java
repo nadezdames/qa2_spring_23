fdda9b0a489ea179203dc51877cd9a0b0389bbe3
neDju <nadezda.meshkova@gmail.com>
2023-02-14 19:03:44 +0200

    Homework #1

---
+++ src/test/java/HomeWorkONE/MyCat.java
@@ -0,0 +1,10 @@
+package HomeWorkONE;
+
+public class MyCat {
+    private String catName = "BiMO";
+    private int catAge = 9;
+    private String catColor = "White, Brown and Ginger";
+    private boolean adopted = true;
+    private long daysTogether = 2920L;
+    private double yearsWePlaningLiveTogether = 999.999999999999999999999999999999999;
+}
