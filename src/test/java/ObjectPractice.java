b860d65c6bf8f92b970b9e1c9fa019e52caf13bb
neDju <nadezda.meshkova@gmail.com>
2023-02-14 20:12:05 +0200

    Class work

---
+++ src/test/java/ObjectPractice.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class ObjectPractice {
+    @Test
+    public void workingWithOrders() {
+        Order firstOrder = new Order();
+        firstOrder.setTotalPrice(22.15);
+        firstOrder.setAddress("Brivibas str. 22");
+        firstOrder.setItemCount(9);
+
+        Order secondOrder = new Order();
+        secondOrder.setTotalPrice(14.20);
+        secondOrder.setAddress("Lubanas str. 16");
+        secondOrder.setItemCount(2);
+
+        System.out.println("Hello, world!");
+        System.out.println("1st Order");
+        System.out.println(firstOrder.getTotalPrice());
+
+        System.out.println("2nd Order");
+        System.out.println(secondOrder.getTotalPrice());
+
+}
+}
