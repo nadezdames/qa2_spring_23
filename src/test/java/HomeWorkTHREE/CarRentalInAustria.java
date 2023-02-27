10766234f024b28f3f0f03a45f1b3ff3cf500ef3
neDju <nadezda.meshkova@gmail.com>
2023-02-27 22:14:45 +0200

    Homework #3

---
+++ src/test/java/HomeWorkTHREE/CarRentalInAustria.java
@@ -0,0 +1,56 @@
+package HomeWorkTHREE;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+// 1. Найти на главное странице список регионов 2. прописать локаторы 3. Вывести к консоль список доступных регионов на главной странице
+public class CarRentalInAustria {
+    private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@id='onetrust-accept-btn-handler']");
+    private final By LIST_OF_COUNTRIES = By.xpath(".//div[@class='tl-box']");
+
+    private final By AUSTRIA = By.xpath(".//a[@href='https://www.discovercars.com/austria']");
+
+    private final By LOCATIONS_IN_TLBOX = By.tagName("a");
+
+    @Test
+    public void confirmAustriaPage() {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+        WebDriver chromeBrowser = new ChromeDriver();
+        chromeBrowser.manage().window().maximize();
+        chromeBrowser.get("http://www.discovercars.com");
+
+        WebDriverWait wait = new WebDriverWait(chromeBrowser, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
+        chromeBrowser.findElement(ACCEPT_COOKIES_BTN).click();
+////        -------------------------------(просто на память)-------------------------------
+//        var listOfCountries = chromeBrowser.findElements(LIST_OF_COUNTRIES);
+//        for (int i = 0; i < listOfCountries.size(); i++) {                                 // ++ это увеличение себя на один (0+1 потом 1+1 потом 2+1 и т.д. ; Если 0+1 , тут 0 это позиция на которой мы на данный момент, а 1 это то, на сколько нам надо увеличится, чтоб попасть на след. позицию
+//            var country = listOfCountries.get(i);
+//            var austria = country.findElements(AUSTRIA);
+//
+//            if (austria != null)
+//                austria.click();
+//            country.findElement(AUSTRIA).click();
+//        }
+////        -------------------------------(просто на память)-------------------------------
+
+//        WebElement austria = chromeBrowser.findElement(AUSTRIA);
+//        austria.click();
+
+        List<WebElement> listOfCountries = chromeBrowser.findElements(LIST_OF_COUNTRIES);
+        for (WebElement s : listOfCountries) {
+           WebElement l = s.findElement(LOCATIONS_IN_TLBOX);
+            System.out.println(l.getText());
+        }
+    }
+
+}
---
+++ src/test/java/HomeWorkTHREE/CarRentalInLatvia.java
@@ -0,0 +1,56 @@
+package HomeWorkTHREE;
+
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+// 1. Найти на главное странице лат регион 2. Кликнуть 3. Проверить,что переключилось на лат регион
+public class CarRentalInLatvia {
+
+    private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@id='onetrust-accept-btn-handler']");
+    private final By CHOSE_LANGUAGE = By.xpath("//*[contains(@class, 'choose-language')]");
+    private final By LV_LANGUAGE = By.xpath(".//a[@data-iso-code= 'lv']");
+    private final By LV_REGION = By.xpath(".//a[text()='Latvija']");
+    private final By LV_TEXT = By.xpath(".//h1[text()='Auto noma Latvijā']");
+
+    @Test
+    public void confirmLvPage() {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+        WebDriver chromeBrowser = new ChromeDriver();
+        chromeBrowser.manage().window().maximize();
+        chromeBrowser.get("http://www.discovercars.com");
+
+        WebDriverWait wait = new WebDriverWait(chromeBrowser, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
+        chromeBrowser.findElement(ACCEPT_COOKIES_BTN).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(CHOSE_LANGUAGE));
+        chromeBrowser.findElement(CHOSE_LANGUAGE).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(LV_LANGUAGE));
+        chromeBrowser.findElement(LV_LANGUAGE).click();
+
+        Actions actionScroll = new Actions(chromeBrowser);
+        actionScroll.scrollByAmount(0, 2000).perform(); // scroll for 2000px
+        wait.until(ExpectedConditions.elementToBeClickable(LV_REGION));
+        chromeBrowser.findElement(LV_REGION).click();
+
+
+        if (chromeBrowser.getPageSource().contains("Auto noma Latvijā")) {
+            System.out.println("LV text present");
+        } else {
+            System.out.println("LV text missing");
+        }
+
+
+    }
+}
